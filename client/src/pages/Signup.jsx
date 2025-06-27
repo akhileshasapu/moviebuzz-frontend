f4292c40e541d0cc920956f82de3a42dc39f406c
akhileshasapu <156326254+akhileshasapu@users.noreply.github.com>
2025-06-27 18:45:26 +0530

    Update Signup.jsx

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -2,10 +2,11 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 function Signup() {
   const [form, setForm] = useState({ username: "", email: "", password: "" });
+    const [loading, setLoading] = useState(false);
    const navigaete = useNavigate()
   const handleSubmit = async (e) => {
     e.preventDefault();
-
+     setLoading(true);
     const res = await fetch("https://movierbuzz-backend.onrender.com/api/auth/signup", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
@@ -24,7 +25,7 @@ function Signup() {
       <input placeholder="Username" onChange={(e) => setForm({ ...form, username: e.target.value })} />
       <input placeholder="Email" onChange={(e) => setForm({ ...form, email: e.target.value })} />
       <input type="password" placeholder="Password" onChange={(e) => setForm({ ...form, password: e.target.value })} />
-      <button type="submit">Signup</button>
+      <button disabled={loading} type="submit">{loading ? "Signing up..." : "Sign Up"}</button>
     </form>
       </div>
     </div>
